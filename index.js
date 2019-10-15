8c84bd076da267c771ac131f1047ce68de1eaa8b
Anders <jaekroll@outlook.com>
2019-10-15 12:02:30 +0200

    skren en ting på tekst

--- index.js
+++ index.js
@@ -4,38 +4,22 @@ const axios = require('axios')
 const app = express()
 const port = 3000
 
-
-app.get('/', (req, res) => {
-    console.log("marshalla");
-    sendtoslack();
-    res.send('Hello World!')
+app.post('/', (req, res) => {
+    console.log("User searched for: " + req.query.text);
+    res.send(sendtoslack())
 })
 
-async function sendtoslack(){
-    console.log("test");
+function sendtoslack(){
     const data = {
-        channel: '#homm3',
-        username: 'Heroes Turn Helper',
-        text: "teksttddest",
-        icon_emoji: ':homm3:',
+        response_type: "in_channel",
         attachments: [
             {
                 "image_url": "https://media.giphy.com/media/RtsDw93ouK74Y/giphy.gif",
-                
             }
         ]        
       };
-      const testUrl =
-        'https://hooks.slack.com/services/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';
-      const url =
-        'https://hooks.slack.com/services/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';    
-    try {
-        
-        const response = await axios.post(url, JSON.stringify(data))
-    }
-    catch(error){
-        console.log ("catchanders", error);
-    }
+
+      return data
 }
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))   
