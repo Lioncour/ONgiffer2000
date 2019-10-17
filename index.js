06dec30e2c81c77c7f526aa489a1192e26b6708c
Anders <jaekroll@outlook.com>
2019-10-17 14:40:41 +0200

    Check request body

--- index.js
+++ index.js
@@ -5,16 +5,31 @@ const app = express()
 const port = 3000
 
 app.post('/', (req, res) => {
-    console.log("User searched for: " + req.query.text);
-    res.send(sendtoslack())
+    console.log("User searched for: " + req.body.text);
+    let url = textToURL(req.body.text)
+
+    if (url === null) {
+        res.status(404).send('Not found');
+    } else {
+        res.send(sendtoslack(url))
+    }
 })
 
-function sendtoslack(){
+function textToURL(text) {
+    switch (text) {
+        case "techlead":
+            return "https://media.giphy.com/media/RtsDw93ouK74Y/giphy.gif"
+        default:
+            return null
+        }
+}
+
+function sendtoslack(url){
     const data = {
         response_type: "in_channel",
         attachments: [
             {
-                "image_url": "https://media.giphy.com/media/RtsDw93ouK74Y/giphy.gif",
+                "image_url": url,
             }
         ]        
       };
