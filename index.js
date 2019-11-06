86aec10e372c16fd6f52c68bf573e6e773f93969
Anders <jaekroll@outlook.com>
2019-11-06 15:08:15 +0100

    fiksa sting 3

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const axios = require('axios')
 const app = express()
 const port = 3000
 app.use(express.urlencoded())
+app.use(express.static('giffer'))
 const gifs = {
-    "techlead":"https://media.giphy.com/media/RtsDw93ouK74Y/giphy.gif"
+    "techlead":"http://157.245.106.185/giffer/KIM.gif"
 
 }
 app.post('/', (req, res) => {
