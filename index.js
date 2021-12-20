7081fae381d26629d9fa084b4635f663ff0874b5
isaksolheim <isak.m.h.s@gmail.com>
2021-12-20 13:08:09 +0100

    Update port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.use(express.urlencoded());
 app.use(express.static('giffer'));
 const gifs = {
